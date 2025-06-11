8f21fcccc44d58d8d70914b8cf121d85811dd819
RubyEsme <rubyelc0911@gmail.com>
2025-06-10 23:59:41 -0600

    Server writing basic

--- starting.go
+++ starting.go
@@ -1,7 +1,14 @@
 package main
-
-import "log"
-
+import (
+	"fmt"
+	"net/http" //this is the library for supporting servers
+)
 func main(){
-	log.Printf("Hello world")	
+	server := &http.Server{ //this is how you write a server here
+		Addr : ":8080", //port
+	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Ok")
+	})
+	server.ListenAndServe() //and this is how you 
 }
